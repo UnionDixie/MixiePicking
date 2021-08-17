5456a428c872ebd8b79aefb27229726c12348c90
UnionDixie <77199764+UnionDixie@users.noreply.github.com>
2021-08-17 03:45:41 +0300

    hot fix3

--- MixiePicking/src/main.cpp
+++ MixiePicking/src/main.cpp
@@ -1,10 +1,28 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSurfaceFormat>
+#include <QOpenGLContext>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSurfaceFormat fmt;
+    fmt.setDepthBufferSize(24);
+
+    // Request OpenGL 3.3 core or OpenGL ES 3.0.
+    if (QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL) {
+        qDebug("Requesting 3.3 core context");
+        fmt.setVersion(3, 3);
+        fmt.setProfile(QSurfaceFormat::CoreProfile);
+    } else {
+        qDebug("Requesting 3.0 context");
+        fmt.setVersion(3, 0);
+    }
+
+    QSurfaceFormat::setDefaultFormat(fmt);
+
 
     MainWindow mainWindow;
     mainWindow.show();

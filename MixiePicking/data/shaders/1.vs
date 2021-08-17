9f7c573d49067e3559ddebf360d17c20095570d7
UnionDixie <77199764+UnionDixie@users.noreply.github.com>
2021-08-17 04:03:43 +0300

    hot fix4

--- MixiePicking/data/shaders/1.vs
+++ MixiePicking/data/shaders/1.vs
@@ -1,8 +1,7 @@
-#version 150
-in vec4 vertex;
-in vec3 normal;
-out vec3 vert;
-out vec3 vertNormal;
+attribute vec4 vertex;
+attribute vec3 normal;
+varying vec3 vert;
+varying vec3 vertNormal;
 uniform mat4 projMatrix;
 uniform mat4 mvMatrix;
 uniform mat3 normalMatrix;
@@ -11,3 +10,4 @@ void main() {
    vertNormal = normalMatrix * normal;
    gl_Position = projMatrix * mvMatrix * vertex;
 };
+
--- MixiePicking/data/shaders/2.fs
+++ MixiePicking/data/shaders/2.fs
@@ -1,7 +1,5 @@
-#version 150
-in highp vec3 vert;
-in highp vec3 vertNormal;
-out highp vec4 fragColor;
+varying highp vec3 vert;
+varying highp vec3 vertNormal;
 uniform highp vec3 lightPos;
 uniform highp vec3 fastOutLine;
 void main() {
@@ -9,5 +7,5 @@ void main() {
    highp float NL = max(dot(normalize(vertNormal), L), 0.0);
    highp vec3 color = vec3(0.39, 1.0, 0.0);
    highp vec3 col = clamp(color * 0.2 + color * 0.8 * NL, 0.0, 1.0);
-   fragColor = vec4(vert + fastOutLine, 1.0);
+   gl_FragColor = vec4(vert + fastOutLine, 1.0);
 };
